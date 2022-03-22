add10ae9514c94df17c6a50afade50c3a8da7e64
Fungus-Light <542461670@qq.com>
2022-03-22 21:00:44 +0800

    design the page

--- Tools/DataEditor/SRC/index.js
+++ Tools/DataEditor/SRC/index.js
@@ -1,6 +1,12 @@
 const ElementUI = require("element-ui")
 
 Vue.use(ElementUI.Col)
+Vue.use(ElementUI.Menu)
+Vue.use(ElementUI.Table)
+Vue.use(ElementUI.TableColumn)
+Vue.use(ElementUI.MenuItem)
+Vue.use(ElementUI.Card)
+
 Vue.use(vueNcform, { extComponents: ncformStdComps, /*lang: 'zh-cn'*/ });
 
 // Bootstrap the app
@@ -8,13 +14,43 @@ new Vue({
     el: '#App',
     data: {
         formSchema: {
-            type: 'object',
-            properties: {
-                name: {
-                    type: 'string'
+            "type": "object",
+            "properties": {
+                "ID": {
+                    "type": "string"
+                },
+                "Key": {
+                    "type": "string"
+                },
+                "Desc": {
+                    "type": "integer"
+                },
+                "Value1": {
+                    "type": "boolean"
                 }
             }
-        }
+        },
+        tableData: [{
+            ID: 1,
+            Key: 'A',
+            Desc: 'desc1'
+        }, {
+            ID: 2,
+            Key: 'B',
+            Desc: 'desc2'
+        }, {
+            ID: 3,
+            Key: 'C',
+            Desc: 'desc3'
+        }, {
+            ID: 4,
+            Key: 'D',
+            Desc: 'desc4'
+        }, {
+            ID: 5,
+            Key: 'E',
+            Desc: 'desc5'
+        },]
     },
     methods: {
         submit() {
@@ -23,6 +59,13 @@ new Vue({
                     // do what you like to do
                 }
             });
-        }
+        },
+        handleOpen(key, keyPath) {
+            console.log(key, keyPath);
+        },
+        handleClose(key, keyPath) {
+            console.log(key, keyPath);
+        },
+
     }
 });
