2671260d4bfafcf15085692e3fd1a5b26a464c27
luuuuyang <luuuuyang@163.com>
2022-05-15 20:43:27 +0800

    Update KCC

--- Assets/Scripts/KCC/ICharacterController.cs
+++ Assets/Scripts/KCC/ICharacterController.cs
@@ -6,6 +6,10 @@ namespace KCC
 {
     public interface ICharacterController
     {
-        
+        void UpdateRotation(ref Quaternion currentRotation, float deltaTime);
+        void UpdateVelocity(ref Vector3 currentVelocity, float deltaTime);
+        void BeforeCharacterUpdate(float deltaTime);
+        void PostGroundingUpdate(float deltaTime);
+        void AfterCharacterUpdate(float deltaTime);
     }
 }
--- Assets/Scripts/KCC/KinematicCharacterMotor.cs
+++ Assets/Scripts/KCC/KinematicCharacterMotor.cs
@@ -17,6 +17,21 @@ namespace KCC
         Standard,
         Extra,
     }
+    [Serializable]
+    public struct KinematicCharacterMotorState
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+        public Vector3 baseVelocity;
+
+        public bool mustUnground;
+        public float mustUngroundTime;
+        public bool lastMovementIterationFoundAnyGround;
+        public CharacterTransientGroundingReport groundingStatus;
+
+        public Rigidbody attachedRigidbody;
+        public Vector3 attachedRigidbodyVelocity;
+    }
     public struct OverlapResult
     {
         public Vector3 normal;
@@ -78,6 +93,7 @@ namespace KCC
         [Header("Components")]
         public CapsuleCollider capsuleCollider;
 
+        // todo: don't need these
         [Header("Capsule Settings")]
         [SerializeField]
         private PhysicMaterial capsulePhysicsMaterial;
@@ -109,6 +125,8 @@ namespace KCC
         public float maxDenivelationAngle = 180f;
 
         [Header("Rigidbody Interaction Settings")]
+        public bool interactiveRigidbodyHandling = true;
+
         [Header("Constraints Settings")]
         public bool hasPlanarConstraint = false;
         public Vector3 planarConstraintAxis = Vector3.forward;
@@ -127,39 +145,93 @@ namespace KCC
         [NonSerialized]
         public LayerMask collidableLayers = -1;
 
-        public Vector3 transientPosition { get { return _transientPosition; } }
+        public Vector3 transientPosition
+        {
+            get { return transientPosition; }
+            private set { transientPosition = value; }
+        }
         public Quaternion transientRotation
         {
-            get { return _transientRotation; }
+            get { return transientRotation; }
             private set
             {
-                _transientRotation  = value;
-                _characterUp        = _transientRotation * Vector3.up;
-                _characterForward   = _transientRotation * Vector3.forward;
-                _characterRight     = _transientRotation * Vector3.right;
+                transientRotation  = value;
+                characterUp        = transientRotation * Vector3.up;
+                characterForward   = transientRotation * Vector3.forward;
+                characterRight     = transientRotation * Vector3.right;
             }
         }
-        public Vector3 velocity { get { return baseVelocity + _attachedRigidbodyVelocity; } }
-        public Vector3 characterUp { get { return _characterUp; } }
-        public Vector3 characterForward { get { return _characterForward; } }
-        public Vector3 characterRight { get { return _characterRight; } }
+        public Vector3 velocity
+        {
+            get { return baseVelocity + attachedRigidbodyVelocity; }
+        }
+        public Vector3 characterUp
+        {
+            get { return characterUp; }
+            private set { characterUp = value; }
+        }
+        public Vector3 characterForward
+        {
+            get { return characterForward; }
+            private set { characterForward = value; }
+        }
+        public Vector3 characterRight
+        {
+            get { return characterRight; }
+            private set { characterRight = value; }
+        }
+        public Vector3 initialSimulationPosition
+        {
+            get { return initialSimulationPosition; }
+            private set { initialSimulationPosition = value; }
+        }
+        public Quaternion initialSimulationRotation
+        {
+            get { return initialSimulationRotation; }
+            private set { initialSimulationRotation = value; }
+        }
+        public Rigidbody attachedRigidbody
+        {
+            get { return attachedRigidbody; }
+            private set { attachedRigidbody = value; }
+        }
+        public Vector3 attachedRigidbodyVelocity
+        {
+            get { return attachedRigidbodyVelocity; }
+            private set { attachedRigidbodyVelocity = value; }
+        }
+        public Vector3 characterTransformToCapsuleTop
+        {
+            get { return characterTransformToCapsuleTop; }
+            private set { characterTransformToCapsuleTop = value; }
+        }
+        public Vector3 characterTransformToCapsuleTopHemi
+        {
+            get { return characterTransformToCapsuleTopHemi; }
+            private set { characterTransformToCapsuleTopHemi = value; }
+        }
+        // public Vector3 characterTransformToCapsuleCenter
+        // {
+        //     get { return characterTransformToCapsuleCenter; }
+        //     private set { characterTransformToCapsuleCenter = value; }
+        // }
+        public Vector3 characterTransformToCapsuleBottomHemi
+        {
+            get { return characterTransformToCapsuleBottomHemi; }
+            private set { characterTransformToCapsuleBottomHemi = value; }
+        }
+        public Vector3 characterTransformToCapsuleBottom
+        {
+            get { return characterTransformToCapsuleBottom; }
+            private set { characterTransformToCapsuleBottom = value; }
+        }
+        public int overlapsCount
+        {
+            get { return overlapsCount; }
+            private set { overlapsCount = value; }
+        }
+        public readonly OverlapResult[] overlaps = new OverlapResult[maxRigidbodyOverlapsCount];
 
-        private Vector3 _transientPosition;
-        private Quaternion _transientRotation;
-        private Vector3 _characterUp;
-        private Vector3 _characterForward;
-        private Vector3 _characterRight;
-        private Vector3 _initialSimulationPosition;
-        private Quaternion _initialSimulationRotation;
-        private Rigidbody _attachedRigidbody;
-        private Vector3 _attachedRigidbodyVelocity;
-        private Vector3 _characterTransformToCapsuleTop;
-        private Vector3 _characterTransformToCapsuleTopHemi;
-        private Vector3 _characterTransformToCapsuleCenter;
-        private Vector3 _characterTransformToCapsuleBottomHemi;
-        private Vector3 _characterTransformToCapsuleBottom;
-        private int _overlapsCount;
-        private List<OverlapResult> _overlaps = new List<OverlapResult>(maxRigidbodyOverlapsCount);
 
         [NonSerialized]
         public ICharacterController characterController;
@@ -178,19 +250,23 @@ namespace KCC
 
 
         // Private
-        
-        private bool _solveMovementCollision = true;
-        private bool _solveGrounding = true;
-        private bool _movePositionDirty = false;
-        private Vector3 _movePositionTarget = Vector3.zero;
-        private bool _moveRotationDirty = false;
-        private Quaternion _moveRotationTarget = Quaternion.identity;
-        
-
-
+        private Collider[] probedColliders = new Collider[maxCollisionBudget];
+        private List<Rigidbody> rigidbodiesPushedThisMove = new List<Rigidbody>(16);
+        private bool solveMovementCollision = true;
+        private bool solveGrounding = true;
+        private bool movePositionDirty = false;
+        private Vector3 movePositionTarget = Vector3.zero;
+        private bool moveRotationDirty = false;
+        private Quaternion moveRotationTarget = Quaternion.identity;
+        private bool lastSolvedOverlapNormalDirty = false;
+        private Vector3 lastSolvedOverlapNormal = Vector3.forward;
+        private int rigidbodyProjectionHitCount = 0;
+        private bool mustUnground = false;
+        private float mustUngroundTimeCounter = 0f;
         
 
         // Constants
+        public const int maxCollisionBudget = 16;
         public const int maxRigidbodyOverlapsCount = 16;
 
         private void OnEnable()
@@ -208,18 +284,18 @@ namespace KCC
         }
         public void SetMovementCollisionSolvingActive(bool value)
         {
-            _solveMovementCollision = value;
+            solveMovementCollision = value;
         }
         public void SetGroundSolvingActive(bool value)
         {
-            _solveGrounding = value;
+            solveGrounding = value;
         }
 
         public void SetPosition(Vector3 position, bool bypassInterpolation = true)
         {
             transform.position = position;
-            _initialSimulationPosition = position;
-            _transientPosition = position;
+            initialSimulationPosition = position;
+            transientPosition = position;
             if (bypassInterpolation)
             {
                 initialTickPosition = position;
@@ -228,7 +304,7 @@ namespace KCC
         public void SetRotation(Quaternion rotation, bool bypassInterpolation = true)
         {
             transform.rotation = rotation;
-            _initialSimulationRotation = rotation;
+            initialSimulationRotation = rotation;
             transientRotation = rotation;
             if (bypassInterpolation)
             {
@@ -238,9 +314,9 @@ namespace KCC
         public void SetPositionAndRotation(Vector3 position, Quaternion rotation, bool bypassInterpolation = true)
         {
             transform.SetPositionAndRotation(position, rotation);
-            _initialSimulationPosition = position;
-            _initialSimulationRotation = rotation;
-            _transientPosition = position;
+            initialSimulationPosition = position;
+            initialSimulationRotation = rotation;
+            transientPosition = position;
             transientRotation = rotation;
             if (bypassInterpolation)
             {
@@ -251,13 +327,201 @@ namespace KCC
 
         public void MoveCharacter(Vector3 toPosition)
         {
-            _movePositionDirty = true;
-            _movePositionTarget = toPosition;
+            movePositionDirty = true;
+            movePositionTarget = toPosition;
         }
         public void RotateCharacter(Quaternion toRotation)
         {
-            _moveRotationDirty = true;
-            _moveRotationTarget = toRotation;
+            moveRotationDirty = true;
+            moveRotationTarget = toRotation;
+        }
+
+        public KinematicCharacterMotorState GetState()
+        {
+            KinematicCharacterMotorState state = new KinematicCharacterMotorState();
+            state.position = transientPosition;
+            state.rotation = transientRotation;
+            state.baseVelocity = baseVelocity;
+            state.attachedRigidbodyVelocity = attachedRigidbodyVelocity;
+            state.mustUnground = mustUnground;
+            state.mustUngroundTime = mustUngroundTimeCounter;
+            state.lastMovementIterationFoundAnyGround = lastMovementIterationFoundAnyGround;
+            state.groundingStatus.CopyFrom(groundingStatus);
+            state.attachedRigidbody = attachedRigidbody;
+            return state;
+        }
+        public void SetState(KinematicCharacterMotorState state, bool bypassInterpolation = true)
+        {
+            SetPositionAndRotation(state.position, state.rotation, bypassInterpolation);
+            baseVelocity = state.baseVelocity;
+            attachedRigidbodyVelocity = state.attachedRigidbodyVelocity;
+            mustUnground = state.mustUnground;
+            mustUngroundTimeCounter = state.mustUngroundTime;
+            lastMovementIterationFoundAnyGround = state.lastMovementIterationFoundAnyGround;
+            groundingStatus.CopyFrom(state.groundingStatus);
+            attachedRigidbody = state.attachedRigidbody;
+        }
+
+        public void SetCapsuleDimension(float radius, float height, float yOffset)
+        {
+            height = Mathf.Max(height, (radius * 2f) + 0.01f);
+
+            capsuleRadius = radius;
+            capsuleHeight = height;
+            capsuleYOffset = yOffset;
+
+            capsuleCollider.radius = capsuleRadius;
+            capsuleCollider.height = capsuleHeight;
+            // capsuleCollider.height = Mathf.Clamp(capsuleHeight, capsuleRadius * 2f, capsuleHeight);
+            capsuleCollider.center = new Vector3(0f, capsuleYOffset, 0f);
+
+            characterTransformToCapsuleTop = capsuleCollider.center + 0.5f * capsuleCollider.height * Vector3.up;
+            characterTransformToCapsuleTopHemi = characterTransformToCapsuleTop + capsuleCollider.radius * Vector3.down;
+            characterTransformToCapsuleBottom = capsuleCollider.center + 0.5f * capsuleCollider.height * Vector3.down;
+            characterTransformToCapsuleBottomHemi = characterTransformToCapsuleBottom + capsuleCollider.radius * Vector3.up;
+        }
+
+        private void Awake()
+        {
+            transientPosition = transform.position;
+            transientRotation = transform.rotation;
+
+            collidableLayers = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                if (!Physics.GetIgnoreLayerCollision(this.gameObject.layer, i))
+                {
+                    collidableLayers |= (1 << i);
+                }
+            }
+
+            SetCapsuleDimension(capsuleRadius, capsuleHeight, capsuleYOffset);    
+        }
+
+        public void UpdatePhase1(float deltaTime)
+        {
+            if (float.IsNaN(baseVelocity.x) || float.IsNaN(baseVelocity.x) || float.IsNaN(baseVelocity.z))
+            {
+                baseVelocity = Vector3.zero;
+            }
+            if (float.IsNaN(attachedRigidbodyVelocity.x) || float.IsNaN(attachedRigidbodyVelocity.x) || float.IsNaN(attachedRigidbodyVelocity.z))
+            {
+                attachedRigidbodyVelocity = Vector3.zero;
+            }
+            
+#if UNITY_EDITOR
+            if (!Mathf.Approximately(transform.lossyScale.x, 1f) || !Mathf.Approximately(transform.lossyScale.y, 1f) || !Mathf.Approximately(transform.lossyScale.z, 1f))
+            {
+                Debug.LogError("Character's lossy scale is not (1,1,1). This is not allowed. Make sure the character's transform and all of its parents have a (1,1,1) scale.", this.gameObject);
+            }
+#endif
+            rigidbodiesPushedThisMove.Clear();
+
+            characterController.BeforeCharacterUpdate(deltaTime);
+
+            transientPosition = transform.position;
+            transientRotation = transform.rotation;
+            initialSimulationPosition = transientPosition;
+            initialSimulationRotation = transientRotation;
+            rigidbodyProjectionHitCount = 0;
+            overlapsCount = 0;
+            lastSolvedOverlapNormalDirty = false;
+
+            if (movePositionDirty)
+            {
+                if (solveMovementCollision)
+                {
+                    Vector3 tmpVelocity = GetVelocityFromMovement(movePositionTarget - transientPosition, deltaTime);
+                    if (InternalCharacterMove(ref tmpVelocity, deltaTime))
+                    {
+                        if (interactiveRigidbodyHandling)
+                        {
+                            ProcessVelocityForRigidbodyHits(ref tmpVelocity, deltaTime);
+                        }
+                    }
+                }
+                else
+                {
+                    transientPosition = movePositionTarget;
+                }
+                movePositionDirty = false;
+            }
+
+            lastGroundingStatus.CopyFrom(groundingStatus);
+            groundingStatus = new CharacterGroundingReport();
+            groundingStatus.groundNormal = characterUp;
+
+            if (solveMovementCollision)
+            {
+                Vector3 resolutionDirection = Vector3.up;
+                float resolutionDistance = 0f;
+                int iterationsMade = 0;
+                bool overlapSolved = false;
+                while (iterationsMade < maxDecollisionIterations && !overlapSolved)
+                {
+                    int nbOverlaps = CharacterCollisionOverlap(transientPosition, transientRotation, probedColliders);
+                    if (nbOverlaps > 0)
+                    {
+
+                    }
+                    else
+                    {
+                        overlapSolved = true;
+                    }
+                    iterationsMade++;
+                }
+            }
+
+            if (solveGrounding)
+            {
+                if (MustUnground())
+                {
+
+                }
+                else
+                {
+
+                }
+                
+            }
+            lastMovementIterationFoundAnyGround = false;
+            if (mustUngroundTimeCounter > 0f)
+            {
+                mustUngroundTimeCounter -= deltaTime;
+            }
+            mustUnground = false;
+
+            if (solveGrounding)
+            {
+                characterController.PostGroundingUpdate(deltaTime);
+            }
+
+            if (interactiveRigidbodyHandling)
+            {
+                
+            }
+        }
+        public void UpdatePhase2(float deltaTime)
+        {}
+        public bool MustUnground()
+        {
+            return mustUnground || mustUngroundTimeCounter > 0f;
+        }
+        private bool InternalCharacterMove(ref Vector3 transientVelocity, float deltaTime)
+        {
+            return true;
+        }
+        private void ProcessVelocityForRigidbodyHits(ref Vector3 processedVelocity, float deltaTime)
+        {}
+        public Vector3 GetVelocityFromMovement(Vector3 movement, float deltaTime)
+        {
+            if (deltaTime <= 0f)
+                return Vector3.zero;
+            return movement / deltaTime;
+        }
+        public int CharacterCollisionOverlap(Vector3 position, Quaternion rotation, Collider[] overlappedColliders, float inflate = 0f, bool acceptOnlyStableGroundLayer = false)
+        {
+            return 1;
         }
     }
 }
